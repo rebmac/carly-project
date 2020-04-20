9e9d309d024ca78bc78ddf3da1c6c80db47bc059
Rebecca MacDonald <rebecca@Rebeccas-MacBook-Pro.local>
2020-04-20 16:46:46 -0400

    first files

---
+++ login.js
@@ -0,0 +1,7 @@
+function onSubmit() {
+    if (document.getElementById('pass').value == 'bigboobz') {
+      window.location.href = "index.html";
+    } else {
+      alert('Access Denied, Please try again');
+    }
+  }
