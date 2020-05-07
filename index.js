20e5ec71dd4134da39461c5ebcd9af695cf6d255
Rebecca MacDonald <rebecca@Rebeccas-MacBook-Pro.local>
2020-05-07 15:15:31 -0400

    memes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function onSubmit() {
     // This is the confusing code that gets the hidden html file and replaces it in the HTML
     // It's a bit easier to do in a framework, in pure javascript it looks SUPER weird, I had to look it up myself
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'crnwp_88_86.template.html', true);
+    xhr.open('GET', 'memes.html', true);
     xhr.onreadystatechange = function () {
       if (this.readyState === 4 && this.status === 200) {
         document.body.innerHTML = this.responseText;
